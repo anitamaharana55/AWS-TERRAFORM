053ca13775d81e00f168bc2511582987ebed02ff
Anita Maharana <anitamaharana55@gmail.com>
2024-09-30 17:09:46 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "test-terraform1432"
     key    = "global/s3/terraform.tfstate"
-    region = "eu-central-1"
+    region = "ap-southeast-2"
     encrypt=true
   }
 }
